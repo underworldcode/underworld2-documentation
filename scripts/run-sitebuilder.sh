f30ee1fa792642afc2dfbbac0ba9f2fd8af3bafc
Louis Moresi <louis.moresi@gmail.com>
2018-10-09 17:49:23 +1100

    Re-organising where the converted nb files go so they don't show up on binder

--- scripts/run-sitebuilder.sh
+++ scripts/run-sitebuilder.sh
@@ -6,6 +6,6 @@ cd $(dirname "$0")/..
 
 # One shot build in cwd
 
-find content/docs/notebooks -name \*.ipynb  -print0 | xargs -0 jupyter-nbconvert --to html 
+# find content/docs/notebooks -name \*.ipynb  -print0 | xargs -0 jupyter-nbconvert --to html 
 
 mkdocs build --clean
