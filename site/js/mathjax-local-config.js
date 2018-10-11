7e625a11e684e21bc1507c0f445ef3c6021664e1
Louis Moresi <louis.moresi@gmail.com>
2018-10-11 11:19:13 +1100

    First pass adding the background material (and getting mathjax to work). Some bugs remain.

---
+++ site/js/mathjax-local-config.js
@@ -0,0 +1,13 @@
+/* Set local configuration for mathjax */
+
+MathJax.Hub.Config({
+    extensions: ["tex2jax.js"],
+    jax: ["input/TeX", "output/HTML-CSS"],
+    tex2jax: {
+      inlineMath: [ ['$','$'], ["\\(","\\)"] ],
+      displayMath: [ ['$$','$$'], ["\\[","\\]"] ],
+      processEscapes: true
+    },
+    TeX: { equationNumbers: { autoNumber: "AMS" } },
+    "HTML-CSS": { availableFonts: ["TeX"] }
+  });
